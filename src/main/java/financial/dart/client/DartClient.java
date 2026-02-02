dffe67c90648d8549d36a93afca1674b0222270e
HanGang <rhgksrkd@naver.com>
2026-02-02 13:14:06 +0900

    Remove: 상장기업 사업보고서 로직 제거

--- src/main/java/financial/dart/client/DartClient.java
+++ src/main/java/financial/dart/client/DartClient.java
@@ -1,7 +1,6 @@
 package financial.dart.client;
 
 import financial.dart.config.DartProperties;
-import financial.dart.dto.DartListResponseDto;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -14,31 +13,6 @@ public class DartClient {
     public DartClient(RestClient dartRestClient, DartProperties props) {
         this.restClient = dartRestClient;
         this.props = props;
-    }
-
-    /**
-     * 요구사항 고정:
-     * - bgn_de=19000101
-     * - end_de=20260101
-     * - pblntf_ty=A
-     * - pblntf_detail_ty=A001
-     * - page_count=10
-     */
-    public DartListResponseDto fetchBusinessReportList(String corpCode) {
-        return restClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/list.json")
-                        .queryParam("crtfc_key", props.apiKey())
-                        .queryParam("corp_code", corpCode)
-                        .queryParam("bgn_de", "19000101")
-                        .queryParam("end_de", "20260101")
-                        .queryParam("pblntf_ty", "A")
-                        .queryParam("pblntf_detail_ty", "A001")
-                        .queryParam("page_no", 1)
-                        .queryParam("page_count", 10)
-                        .build())
-                .retrieve()
-                .body(DartListResponseDto.class);
     }
 
     // ✅ document.xml ZIP 다운로드
